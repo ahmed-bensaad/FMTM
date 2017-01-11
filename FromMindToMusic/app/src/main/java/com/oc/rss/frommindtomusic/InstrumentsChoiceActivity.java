26067db5ca3672f383559e5756c2c30ec6fe3686
mgarnier <mathilde.garnier@telecom-paristech.fr>
2017-01-11 10:15:25 +0100

    Ajout activité InstrumentsChoice dans mode Auto d'orchestrate : possibilité de choisir 1 insrument puis Ok renvoie vers ResultActivity

--- FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/InstrumentsChoiceActivity.java
+++ FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/InstrumentsChoiceActivity.java
@@ -1,13 +1,43 @@
 package com.oc.rss.frommindtomusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InstrumentsChoiceActivity extends AppCompatActivity {
+    private Spinner listeInstruments=null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruments_choice);
+
+        listeInstruments = (Spinner) findViewById(R.id.instruments);
+        List<String> instruments = new ArrayList<String>();
+        instruments.add("Piano");
+        instruments.add("Violon");
+        instruments.add("Trompette");
+        instruments.add("Sabre laser");
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, instruments);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        listeInstruments.setAdapter(adapter);
+
+        Button ok= (Button) findViewById(R.id.ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InstrumentsChoiceActivity.this,ResultActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
