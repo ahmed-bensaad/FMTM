30d3699b1a3258e4a73f501978fa31218a7a88fd
chagneux <mathis.chagneux@telecom-paristech.fr>
2017-01-20 17:59:28 +0100

    La méthode temporelle de détection des notes fonctionne mainenant pour des cas réels où les fronts sont très bien dessinés : à partir d'un fichier audio, on arrive à renvoyer un tableau contenant la liste des fréquences et l'instant où elles commencent à être jouées.

---
+++ Module Transcription/Detection note/notes.m
@@ -0,0 +1,14 @@
+function [result] = notes(x,fe)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+fr = front(x) ; 
+N = length(fr) ; 
+F = [] ;
+for i=1:N-1
+    note = fondamental(x(fr(i):fr(i+1)),fe) ;
+    F = [F note] ; 
+end
+F = [F fondamental(x(fr(N):end),fe)] ; 
+result = [fr*(1/fe) ; F]'
+end
+
--- Module Transcription/Détection front/front.m
+++ Module Transcription/Détection front/front.m
@@ -1,14 +1,22 @@
 function [result] = front(x)
-
+%FRONT Cette fonction renvoie la liste des indices des débuts de notes
 N = length(x) ; 
+
 M = floor(N/1000) ;
+
+
 y =  conv([ones(1,M) ones(1,M)*(-1)],conv((ones(1,N)*0.99).^(0:N-1),2*(x.*x))) ;
 z = max(0,y(1:N)) ;
 [M I] = max(z);
-result = z/M ; 
-
-
-
-
+h = z/M ; 
+[pks,locs] = findpeaks(h) ; 
+K = length(locs); 
+R = [] ;
+for i = 1:K
+    if pks(i) > 0.1
+        R = [R locs(i)] ; 
+    end 
+end 
+result = R ;          
 end
 
