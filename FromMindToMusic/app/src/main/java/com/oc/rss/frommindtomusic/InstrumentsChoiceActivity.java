5b586749c80b09259b8323fb5cf306b866e348d3
Ahmed <ahmed.bensaad@telecom-paristech.fr>
2017-01-26 16:48:19 +0100

    changement de bouton add instrument de DIY en des boutons de menu ,ajout de fonctionnalité remove instrument et changement de l'icone de l'appli

--- FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/InstrumentsChoiceActivity.java
+++ FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/InstrumentsChoiceActivity.java
@@ -3,6 +3,8 @@ package com.oc.rss.frommindtomusic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,13 +14,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.oc.rss.frommindtomusic.R.id.app_bar_add;
+import static com.oc.rss.frommindtomusic.R.id.app_bar_remove;
+
 public class InstrumentsChoiceActivity extends AppCompatActivity {
     private  ArrayList<Spinner> spinnersList=new ArrayList<Spinner>();
 
     private Spinner spinner1=null;
     private Spinner spinner2=null;
     int i=2;
-
+    private ArrayAdapter<String> adapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +48,7 @@ public class InstrumentsChoiceActivity extends AppCompatActivity {
         instruments.add("Clarinette");
         instruments.add("Sabre laser");
 
-        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, instruments);
+        adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, instruments);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
 
@@ -63,20 +68,49 @@ public class InstrumentsChoiceActivity extends AppCompatActivity {
 
         });
 
-        Button add= (Button) findViewById(R.id.add);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (i<10){
-                    Spinner spinner=spinnersList.get(i);
-                    spinner.setVisibility(View.VISIBLE);
-                    spinner.setAdapter(adapter);
-                    i++;}
-                else {
-                    Toast.makeText(InstrumentsChoiceActivity.this, "10 instruments max", Toast.LENGTH_SHORT).show();
-                }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.add_menu,m);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch(id){
+            case(app_bar_add): {
+                add();
+                break;
+            }
+            case(app_bar_remove):{
+                remove();
+                break;
             }
 
-        });
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void add(){
+        if (i<10){
+            Spinner spinner=spinnersList.get(i);
+            spinner.setVisibility(View.VISIBLE);
+            spinner.setAdapter(adapter);
+            i++;}
+        else {
+            Toast.makeText(InstrumentsChoiceActivity.this, "10 instruments max", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void remove(){
+        if (i>1){
+            i--;
+            Spinner spinner=spinnersList.get(i);
+            spinner.setVisibility(View.INVISIBLE);
+            spinner.setAdapter(adapter);
+            }
+        else {
+            Toast.makeText(InstrumentsChoiceActivity.this, "1 instrument min", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
