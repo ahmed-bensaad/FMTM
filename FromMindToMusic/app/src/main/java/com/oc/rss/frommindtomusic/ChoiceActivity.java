2ea3e4a0d2b20dbd7809ee6e79616d1c72723a68
Ahmed <ahmed.bensaad@telecom-paristech.fr>
2017-01-26 18:51:05 +0100

    ajout d'un toast 'what else' si on revient a la choiceActivity

--- FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/ChoiceActivity.java
+++ FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/ChoiceActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ChoiceActivity extends AppCompatActivity {
 
@@ -49,6 +50,18 @@ public class ChoiceActivity extends AppCompatActivity {
             }
 
         });
+    }
+    static boolean f=false;
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (f) {
+            Toast.makeText(ChoiceActivity.this, "So, what else ?",
+                    Toast.LENGTH_LONG).show();
+        }
+        else{
+            f=true;
+        }
     }
 
 }
