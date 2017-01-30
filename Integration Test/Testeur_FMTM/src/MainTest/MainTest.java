630a32f04384911d2dff384bcc1447f054463ac9
Ahmed <ahmed.bensaad@telecom-paristech.fr>
2017-01-30 15:17:42 +0100

    Implementation de fonctitons bouchon

--- Integration Test/Testeur_FMTM/src/MainTest/MainTest.java
+++ Integration Test/Testeur_FMTM/src/MainTest/MainTest.java
@@ -5,17 +5,26 @@ import java.util.ArrayList;
 public class MainTest {
 	
 	public static void main(String[] args) {
-			ArrayList trans;
-			trans=Transcription();
+			
+			ArrayList<float[]> trans=Transcription();
 			Synthèse(trans);
 	}
-	public static ArrayList Transcription() {
-		
-		return null;
+	public static ArrayList<float[]> Transcription() {
+		ArrayList<float[]> trans = new ArrayList<float[]>();
+		float[] note1 = {(float) 3.5,(float) 2.5};
+		float[] note2 ={(float) 2.5,(float) 5.5};
+		trans.add(0,note1);
+		trans.add(0,note2);
+
+		return trans;
 	}
-	public static void Synthèse(ArrayList A) {
-		
-		
+	public static void Synthèse(ArrayList<float[]> A) {
+		for (int i=0;i<A.size();i++){
+			for (float j:A.get(i)){
+				System.out.print(j + " ");
+			}
+			System.out.println("");
+		}
 	}
 
 }
