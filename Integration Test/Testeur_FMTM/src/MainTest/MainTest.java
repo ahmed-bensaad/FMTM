af2fbcb8113fa1e4abc18f132da315a0db3fea85
Ahmed <ahmed.bensaad@telecom-paristech.fr>
2017-01-30 14:48:08 +0100

    mise en place de l'environnement de test

---
+++ Integration Test/Testeur_FMTM/src/MainTest/MainTest.java
@@ -0,0 +1,21 @@
+package MainTest;
+
+import java.util.ArrayList;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+			ArrayList trans;
+			trans=Transcription();
+			Synthèse(trans);
+	}
+	public static ArrayList Transcription() {
+		
+		return null;
+	}
+	public static void Synthèse(ArrayList A) {
+		
+		
+	}
+
+}
