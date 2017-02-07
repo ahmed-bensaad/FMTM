d3a6b4808738621fe4cc83c685a2ebbcdeb54593
raphael <raphael.achddou@telecom-paristech.fr>
2017-02-07 10:36:03 +0100

    mise en page de mainActivity provisoire

--- FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/MainActivity.java
+++ FromMindToMusic/app/src/main/java/com/oc/rss/frommindtomusic/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import java.io.File;
@@ -14,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
          f = new File(Environment.getExternalStorageDirectory(), "FMTM");
         if (!f.exists()) {
