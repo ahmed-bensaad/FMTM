42ce0ae6744a85ec04a20345df3b58a22b5bc033
Luc Motte <lucbm1995@gmail.com>
2017-02-27 16:46:14 +0100

    Synthèse, traduction scilab -> java

---
+++ Module Tonalité/Test_audio/src/OscFm.java
@@ -0,0 +1,52 @@
+
+public class OscFm {
+	
+	public static double[] jouer(double[] f, double d, double[] m) // f -> fréquence(t) en Hz, m -> amplitude(t), d -> durée en secondes
+	{
+		int sampleRate = 44100;		// Samples per second
+		double Te = 1.0/sampleRate;
+		
+		// Calculate the number of frames required for specified duration
+		int numFrames = (int) (d * sampleRate);
+		
+		double[] s = new double[numFrames];
+		double[] phi = new double[numFrames];
+		
+		phi[0]=0;
+		for(int i = 1; i<numFrames;i++)
+		{
+			phi[i]=phi[i-1]+2*Math.PI*Te*f[i];
+		}
+		
+		for(int i = 0; i<numFrames;i++)
+		{
+			s[i]=m[i]*Math.cos(phi[i]);
+		}
+		
+		return s;
+	} 
+	
+}
+
+/*  code scilab
+//on veut retourner s(t)=m(t)cos(phi(t)) avec phi(t)=2*pi*(intégrale entre 0 et t de f(t) + f(0)) cf poly. synthèse FM
+pi=3.14159;
+
+Te= 1/Fe; //temps échantillonage
+
+x=[0:Te:d]; // axe des temps
+
+phi=[0:Te:d]; // definission de phi (cf.poly., méthode des rectangles)
+phi(1)=0;
+n=length(phi);
+for i=2:n
+    phi(i)= phi(i-1) + 2*pi*Te*f(i);
+end
+
+
+s=[0:Te:d];
+for i=1:n
+   s(i)= m(i)*cos(phi(i));
+end
+
+*/
---
+++ Module Tonalité/Test_audio/src/Trompette.java
@@ -0,0 +1,139 @@
+public class Trompette {
+
+	public static double[] jouer_note(double fréquence, double durée)
+	{
+		double fv = 4; // fréquence vibrato
+		double a =0.01; // excursion du vibrato
+		int Fe = 44100;
+		double Te = 1.0/Fe;
+		// Calculate the number of frames required for specified duration
+		int numFrames = (int) (durée * Fe);
+		
+		double[] m = new double[numFrames];
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			m[i]=5*fréquence;
+		}
+		
+		double[] f = new double[numFrames]; 
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			f[i]=fréquence;
+		}
+		
+		double[] x1 = OscFm.jouer(f, durée, m);
+		
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			double t= i*Te;
+			x1[i]=x1[i]*Enveloppe.enveloppe_ADSR(t, durée,0.5)+fréquence;
+		}
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			double t= i*Te;
+			x1[i]=x1[i]*(1+Enveloppe.enveloppe_vibrato(t, durée)*Math.sin(2*Math.PI*fv*t));
+		}
+		
+		double[] m2 = new double[numFrames]; 
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			m2[i]=1;
+		}
+		for(int i=0;i<numFrames;i++)
+		{
+			double t= i*Te;
+			m2[i]=m2[i]*(1+Enveloppe.enveloppe_vibrato(t, durée)*Math.sin(2*Math.PI*fv*t));
+		}
+		
+		double[] s = OscFm.jouer(x1, durée, m2);
+		
+		for(int i=0;i<numFrames;i++)
+		{
+			double t = i*Te;
+			s[i]=s[i]*Enveloppe.enveloppe_ADSR(t, durée,0.5);
+		}
+		
+		return s;
+	}
+	
+	public static double[] jouer(double[] N, double[] R)
+	{
+		double fe =44100;
+		double duration = 0;
+		for(int i = 0 ; i<R.length;i++)
+		{
+			duration=duration+R[i];
+		}
+		
+		int numFrames =  (int) (duration*fe); 
+
+		double[] s = new double[numFrames];
+		int k =0;
+		for(int i =0; i<N.length;i++)
+		{
+			double[] y=jouer_note(N[i],R[i]);
+			
+			for(int j =0; j<y.length;j++)
+			{
+				s[k]=y[j];
+				k++;
+			}
+			
+		}
+		return s;
+	}
+}
+/*
+
+function [s]= trompette(fp,fm,Fe,d,m0) // fp -> hauteur, d-> durée,  m0-> amplitude du sinus modulant 
+    
+   Te=1/Fe; // temps echantillonage
+   
+   x=[0:Te:d]; //axe temps
+   
+   m=[0:Te:d];   // amplitude constante au cours du temps
+   n=length(m);
+   for i=1:n
+       m(i)= m0;
+   end
+    
+   f=[0:Te:d];   // frequence de l'oscillateur FM constante au cours du temps
+   for i=1:n
+       f(i)= fm;
+   end 
+   
+   x1 = oscFM(f,m,Fe,d);
+
+   for i=1:n
+       x1(i)= x1(i)+fp;
+   end  
+   
+   m2=[0:Te:d];   // amplitude constante au cours du temps de s
+   n=length(m2);
+   for i=1:n
+       m2(i)= 1;
+   end
+
+   s=oscFM(x1,m2,Fe,d);
+  
+  for i=1:n
+      s(i)=s(i)*enveloppe_ADSR(x(i)); 
+  end
+  B=0;
+  //s=cuivrage(s,B,1/Fe);
+  //plot(s);
+    
+   //sound(s,Fe);
+    
+   // amelioration possible => Jitter, cuivrage, shéma plus complexe de l'instrument à vent
+endfunction
+
+
+
+
+ */
