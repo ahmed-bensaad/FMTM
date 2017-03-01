c29ceb2d053d413d90efaaf36fb01cbc8d5ff2fc
Pierre GASNIER <pierre.gasnier@telecom-paristech.fr>
2017-03-01 13:27:44 +0100

    ajout classe Accord et Lecture

---
+++ Accompagnement/pact21/Accord.java
@@ -0,0 +1,74 @@
+package pact21;
+
+public class Accord {
+	
+	public static String[] accord(String ton, String note){
+		int n = conversionMaj(ton, note);
+		int acc = accompagnement(n);
+		String[] accord = accordMaj(acc,ton);
+		return accord;
+	}
+	public static int conversionMaj(String note, String ton){
+		String[] G={"do", "re", "mi", "fa", "sol", "la", "si"};
+		String[] GM={"do", "re", "mi", "fa", "sol", "la", "si", " ", "reb","mib", "fa#", " ", " ", "sib"};
+		int k =0;
+		int l =0;
+		for(int i =0 ; i<7;i++)
+		{
+			if(G[i].equals(note))
+			{
+				k=i;
+			}
+		}
+		for(int j=0 ; j<14 ; j++)
+		{
+			if(GM[j].equals(ton)){
+				l=j;
+			}
+		}
+		int m=((k-l) % 7);
+		return m;
+	
+	}
+	
+	public static int accompagnement(int n)
+	{
+		int acc =0;
+		switch (n) {
+		case 1: acc=0;
+		break;
+		case 2: acc=4;
+		break;
+		case 3: acc=0;
+		break;
+		case 4: acc=3;
+		break;
+		case 5: acc=4;
+		break;
+		case 6: acc=3;
+		break;
+		case 7: acc=4;
+		break;
+		
+		}
+		return acc;
+	}
+	
+	public static String[] accordMaj(int acc, String ton)
+	{
+		String[] G={"do", "ré", "mi", "fa", "sol", "la", "si"};
+		String[] GM={"do", "ré", "mi", "fa", "sol", "la", "si", " ", "réb","mib", "fa#", " ", " ", "sib"};
+		int l =0;
+		
+		for(int j=0 ; j<14 ; j++)
+		{
+			if(GM[j].equals(ton)){
+				l=j%7;
+			}
+		}
+		int k = (acc+l)%7;
+		String[] notes = {G[k], G[(k+2)%7], G[(k+4)%7]};
+		return notes;
+	}
+
+}
---
+++ Accompagnement/pact21/Lecture.java
@@ -0,0 +1,63 @@
+package pact21;
+import java.lang.Math;
+public class Lecture {
+	
+	
+	public static String[] note(double f)
+	{	
+		double x=Math.log(f/440)/Math.log(Math.pow(2,(1/12)));
+		int y= (int) (Math.round(x) + 45) ;
+		int oct = Math.floorDiv(y,12);
+		int m = y% 12;
+		String[] G={"do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"};
+		String n = G[m+1];
+		String[] d = {Integer.toString(oct), n};
+		return d;
+	}
+	
+	public static String tonaliteMaj(String[] N){
+		int k=0;
+		int l=0;
+		for(int i = 0; i< N.length; i++){
+		    if (N[i].equals("si#"))
+		        k=Math.max(7,k);
+		    else if (N[i].equals("mi#"))
+		            k =Math.max(6,k);
+		        else if (N[i].equals("la#"))
+		                k=Math.max(5,k);
+		            else if (N[i].equals("re#"))
+		                    k=Math.max(4,k);
+		                else if (N[i].equals("sol#"))
+		                        k= Math.max(3,k);
+		                    else if (N[i].equals("do#"))
+		                            k= Math.max(2,k);
+		                        else if (N[i].equals("fa#"))
+		                                k= Math.max(1,k);
+		}
+		for (int j=0; j<N.length; j++){
+			if (N[j].equals("fab"))
+		        l=Math.max(7,l);
+		    else if (N[j].equals("dob"))
+		            l =Math.max(6,l);
+		        else if (N[j].equals("solb"))
+		                l=Math.max(5,l);
+		            else if (N[j].equals("reb"))
+		                    l=Math.max(4,l);
+		                else if (N[j].equals("lab"))
+		                        l= Math.max(3,l);
+		                    else if (N[j].equals("mib"))
+		                            l= Math.max(2,l);
+		                        else if (N[j].equals("sib"))
+		                                l= Math.max(1,l);
+		}
+		if (l > 0)
+		    l=12-l;
+
+		k=Math.max(k,l);    
+		String [] GM={"do", "sol", "re", "la", "mi", "si", "fa#", "reb","lab", "mib","sib","fa"};
+		return GM[k];
+
+
+	}
+
+}
