fcf6700d462e99380dc353ea76cf954fb880dacf
Mathis Chagneux <mathis.chagneux@telecom-paristech.fr>
2017-03-02 09:15:13 +0100

    Le programme JAVA réalise maintenant les mêmes choses que le programme Matlab mais avec une (beaucoup) moins bonne précision et efficacité qui est liée à l'implémentation pour le moment rudimentaire de la fonction de détection de pics. Le programme prend en entrée un ficher .wav et ressort une liste de couples correspondant à la liste des dates des notes et de leurs fréquences. (reverted from commit ee48f9eb39b6f61a17b91e6dff8f52a1c428a2cf)

--- Transcription JAVA/src/core/Main.java
+++ Transcription JAVA/src/core/Main.java
@@ -1,41 +1,23 @@
 package core;
-import java.io.*;
-import java.util.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		// Opens the file and reads frames into a double[], gets the sampling rate 
 		Audio audio = new Audio(args[0]) ; 
 		double[] signal = audio.getSignal(); 
 		double fe = audio.getFe() ; 
+		double[] envelope = Process.envelope(signal) ; 
+		double[] transients = Process.transients(envelope,2000) ; 
+		Visualizer.show(transients,fe);
+
 		
-		// Gets the list of musical events and the number of these events 
-		ArrayList <Integer> tr = TransientDetector.transientLocations(signal, 100, fe) ; 
-		int N = tr.size() ; 
 		
-		// For each of these events, determines the note they correspond to and add it to the text file with the the date of the event 
-		PrintWriter notes = null ; 
-		try{
-			notes = new PrintWriter("notes.txt") ; 
-			for (int i = 0 ; i < N-1 ; i++){ 
-				double[] noteSignal = Arrays.copyOfRange(signal, tr.get(i), tr.get(i+1)) ; 
-				notes.println("(" + tr.get(i)/fe  + ","  + NoteDetector.note(noteSignal, fe)  +")")  ;
-			}
-			double[] noteSignal = Arrays.copyOfRange(signal, tr.get(N-2), tr.get(N-1)) ;
-			notes.println("(" + tr.get(N-1)/fe  + ","  + NoteDetector.note(noteSignal, fe) +")") ; 
-				
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally{
-			try{
-				notes.close() ; 
-			}
-			catch(Exception e) {}
-		}
+		
+			
+		
+		
+		
 		
 	}
 	
