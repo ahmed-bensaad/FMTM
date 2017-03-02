2e774a1aac7841bb4590457f4b58f391c11efea7
Mathis Chagneux <mathis.chagneux@telecom-paristech.fr>
2017-03-02 08:56:31 +0100

    Première amélioration du code de détection des pics (qui ne fonctionne pas encore bien).

---
+++ core/NoteDetector.java
@@ -0,0 +1,24 @@
+package core;
+
+import java.util.*;
+import org.apache.commons.math3.complex.*;
+import org.apache.commons.math3.transform.*;
+
+public class NoteDetector {
+	
+
+
+	public static final double note(double[] input, double fe){ 
+		
+		double[] fft = Utilities.fft(input) ; 
+		ArrayList <Double> fftList = new ArrayList<Double>(fft.length) ; 
+		for (int i = 0 ; i < fft.length ; i++){ 
+			fftList.add(fft[i]) ; 
+		}
+		int maxIndex = fftList.indexOf(Collections.max(fftList)) ; 
+		return (maxIndex*fe)/fft.length ; 
+		
+	}
+
+	
+}
