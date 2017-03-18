e235da7ed72e51e32369982bff59b4de546b0eb1
Mathis Chagneux <mathis.chagneux@telecom-paristech.fr>
2017-03-18 22:15:12 +0100

    Ajout des classes du module accompagnement

--- Java/src/main/Tests.java
+++ Java/src/main/Tests.java
@@ -10,20 +10,23 @@ public class Tests {
 
 	public static void main(String[] args){
 				
+		// ouverture du fichier
 		Audio audio = new Audio(args[0]) ; 
 		double[] signal = audio.getSignal() ; 
 		double fe = audio.getFe(); 
 		
+		// début de la partie transcription
 		int p = (int)(0.5*fe) ; 
 		int n = (int)(0.02*fe) ; 
-		
-		//double[] transients = TransientDetector.transients(signal, n) ; 
-		//Visualizer.temporalShow(signal, fe);
-		//Visualizer.temporalShow(transients, fe);
 		double[][] notes = Notes.notes(signal, fe, n, p) ; 
-		System.out.println("Fin transription");
-		Play.play(notes, "Basse") ;
-		System.out.println("Fin synthèse");
+		// fin de la partie transcription
+		
+		// début de la partie accompagnement
+		// fin de la partie accompagnement
+		
+		// début de la partie synthèse 
+		// fin de la partie synthèse
+
 		
 	}
 
