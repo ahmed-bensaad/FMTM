7d9bbccd466afd189a27702fbb8459d4ce6a955d
Mathis Chagneux <mathis.chagneux@telecom-paristech.fr>
2017-03-20 09:31:57 +0100

    Gestion dépot 2

--- Module Transcription (MATLAB)/Fonctions de visualisation/spectrofrequentiel.m
+++
@@ -1,8 +0,0 @@
-function [ output_args ] = spectrofrequentiel(x,fe)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-x = x(:,1) ; 
-spectrogram(x,[],[],[],fe,'yaxis') 
-
-end
-
--- Module Transcription (MATLAB)/Génération de fonctions/expo.m
+++
@@ -1,8 +0,0 @@
-function [result] = expo(T,a,fe)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-t = 0:1/fe:T
-result = exp(a*t)
-
-end
-
