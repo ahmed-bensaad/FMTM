c85daf8699be829130b4567b18ebbfce95988ebf
Mathis Chagneux <mathis.chagneux@telecom-paristech.fr>
2017-03-20 09:31:17 +0100

    gestion du dépot

--- Module Tonalité/conversionMaj.m
+++
@@ -1,8 +0,0 @@
-function [ note1 ] = conversionMaj(ton, note)
-G={'do', 're', 'mi', 'fa', 'sol', 'la', 'si'}
-GM={'do', 're', 'mi', 'fa', 'sol', 'la', 'si', ' ', 'reb','mib', 'fa#', ' ', ' ', 'sib'};
-i = find(ismember(GM, ton));
-j = find(ismember(G, note));
-note1=mod(j-i,7)+1;
-end
-
--- Module Tonalité/majToMin.m
+++
@@ -1,9 +0,0 @@
-function [ tonMin ] = majToMin( tonMaj )
-%
-GM={'do', 're', 'mi', 'fa', 'sol', 'la', 'si', 'reb','mib', 'fa#', 'sib'};
-i = find(ismember(GM, tonMaj));
-Gm={'la', 'si', 'do#', 're', 'mi', 'fa#', 'sol#', 'sib', 'do', 're#', 'sol'} 
-tonMin=Gm{i}
-
-end
-
